883dd7e05e6f881399f6e22eccbb51deb25f8d2f
disha.javur <disha.javur@socialfinance.org.uk>
2024-05-17 13:08:55 +0100

    adding assets/cin_census cin_incoming to repository.py

--- liiatools_pipeline/repository.py
+++ liiatools_pipeline/repository.py
@@ -2,6 +2,7 @@ from dagster import repository
 from liiatools.common._fs_serializer import register
 
 from liiatools_pipeline.jobs.ssda903 import ssda903_incoming
+from liiatools_pipeline.jobs.cin import cin_incoming
 from liiatools_pipeline.jobs.external_dataset import external_incoming
 from liiatools_pipeline.jobs.sufficiency_903 import ssda903_sufficiency
 from liiatools_pipeline.sensors.location_sensor import location_sensor
@@ -18,7 +19,7 @@ def sync():
     For hints on building your Dagster repository, see our documentation overview on Repositories:
     https://docs.dagster.io/overview/repositories-workspaces/repositories
     """
-    jobs = [ssda903_incoming, external_incoming, ssda903_sufficiency]
+    jobs = [ssda903_incoming, external_incoming, ssda903_sufficiency, cin_incoming]
     schedules = []
     sensors = [location_sensor, sufficiency_sensor]
 
