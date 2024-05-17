8e8044a6f8835ef552ebffd6ff33d1dc2a2bbe0e
disha.javur <disha.javur@socialfinance.org.uk>
2024-05-17 12:37:26 +0100

    adding assets/cin_census

---
+++ liiatools_pipeline/assets/cin_census.py
@@ -0,0 +1,21 @@
+from dagster import asset
+from decouple import config as env_config
+from fs import open_fs
+from liiatools.cin_census_pipeline.spec import load_pipeline_config
+
+
+@asset
+def pipeline_config():
+    return load_pipeline_config()
+
+
+@asset
+def process_folder():
+    output_location = env_config("OUTPUT_LOCATION", cast=str)
+    return open_fs(output_location)
+
+
+@asset
+def incoming_folder():
+    input_location = env_config("INPUT_LOCATION", cast=str)
+    return open_fs(input_location)
---
+++ liiatools_pipeline/repository_cin.py
@@ -0,0 +1,28 @@
+from dagster import repository
+from liiatools.common._fs_serializer import register
+
+from liiatools_pipeline.jobs.cin import cin_incoming
+from liiatools_pipeline.jobs.external_dataset import external_incoming
+from liiatools_pipeline.jobs.sufficiency_903 import ssda903_sufficiency
+from liiatools_pipeline.sensors.location_sensor import location_sensor
+from liiatools_pipeline.sensors.sufficiency_sensor import sufficiency_sensor
+
+register()
+
+
+@repository
+def sync():
+    """
+    The repository definition for this etl Dagster repository.
+
+    For hints on building your Dagster repository, see our documentation overview on Repositories:
+    https://docs.dagster.io/overview/repositories-workspaces/repositories
+    """
+    jobs = [cin]
+    schedules = []
+    sensors = [location_sensor, sufficiency_sensor]
+
+    return jobs + schedules + sensors
+
+
+# sufficiency_sensor
