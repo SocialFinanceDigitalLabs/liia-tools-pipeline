bf5925a8a4aba161a9faa0a1cf3f9c30f1e2636a
Patrick Troy <patrick.troy@socialfinance.org.uk>
2024-07-01 14:19:25 +0100

    add notes

--- liiatools_pipeline/jobs/common.py
+++ liiatools_pipeline/jobs/common.py
@@ -3,7 +3,7 @@ from liiatools_pipeline.ops import common1
 from liiatools_pipeline.assets.common import dataset
 
 @job
-def globals()[f"{dataset}_incoming"]():
+def globals()[f"{dataset}_incoming"]():  # I don't think there is a need to change the name of this function, it could just be "incoming"
     session_folder, session_id, incoming_files = common1.create_session_folder()
     archive = common1.open_archive(session_id)
 
