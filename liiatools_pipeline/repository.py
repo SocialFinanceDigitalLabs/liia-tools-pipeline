0b5d3f32fd2035697a464b800da98606b3b51ecb
Matthew Pugh <pughmds@cyram.org>
2024-07-03 13:40:35 +0100

    Bumped version of sufficiency library and others, upped own version

--- liiatools_pipeline/repository.py
+++ liiatools_pipeline/repository.py
@@ -2,7 +2,6 @@ from dagster import repository
 from liiatools.common._fs_serializer import register
 
 from liiatools_pipeline.jobs.ssda903 import ssda903_incoming
-from liiatools_pipeline.jobs.external_dataset import external_incoming
 from liiatools_pipeline.jobs.sufficiency_903 import ssda903_sufficiency
 from liiatools_pipeline.sensors.location_sensor import location_sensor
 from liiatools_pipeline.sensors.sufficiency_sensor import sufficiency_sensor
@@ -18,7 +17,7 @@ def sync():
     For hints on building your Dagster repository, see our documentation overview on Repositories:
     https://docs.dagster.io/overview/repositories-workspaces/repositories
     """
-    jobs = [ssda903_incoming, external_incoming, ssda903_sufficiency]
+    jobs = [ssda903_incoming, ssda903_sufficiency]
     schedules = []
     sensors = [location_sensor, sufficiency_sensor]
 
