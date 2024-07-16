c150eb84bba3934a0d6c94719ed50af1a200037e
disha.javur <disha.javur@socialfinance.org.uk>
2024-07-16 16:45:26 +0100

    creating a sensor to trigger the move_current job once clean job completes

--- liiatools_pipeline/sensors/move_current_sensor.py
+++ liiatools_pipeline/sensors/move_current_sensor.py
@@ -1,13 +1,12 @@
 from dagster import RunRequest, RunsFilter, DagsterRunStatus, sensor
-from liiatools_pipeline.jobs.common_la import clean as ssda903_clean, move_current as ssda903_move_current
-# from liiatools_pipeline.jobs.common_org import ssda903_sufficiency
+from liiatools_pipeline.jobs.common_la import clean, move_current
 
 
-@sensor(job=ssda903_move_current)
+@sensor(job=move_current)
 def ssda903_move_current_sensor(context):
     run_records = context.instance.get_run_records(
         filters=RunsFilter(
-            job_name=ssda903_clean.name,
+            job_name=clean.name,
             statuses=[DagsterRunStatus.SUCCESS],
         ),
         order_by="update_timestamp",
