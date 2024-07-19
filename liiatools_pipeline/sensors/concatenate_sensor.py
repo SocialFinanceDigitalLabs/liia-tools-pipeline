844aafe810d9a86da25c390da93a3d372f3dda58
disha.javur <disha.javur@socialfinance.org.uk>
2024-07-19 11:21:02 +0100

    creating new sensors

---
+++ liiatools_pipeline/sensors/concatenate_sensor.py
@@ -0,0 +1,22 @@
+from dagster import RunRequest, RunsFilter, DagsterRunStatus, sensor
+from liiatools_pipeline.jobs.common_la import move_current, concatenate
+
+
+@sensor(job=concatenate)
+def concatenate_sensor(context):
+    run_records = context.instance.get_run_records(
+        filters=RunsFilter(
+            job_name=move_current.name,
+            statuses=[DagsterRunStatus.SUCCESS],
+        ),
+        order_by="update_timestamp",
+        ascending=False,
+    )
+
+    if run_records:  # Ensure there is at least one run record
+        latest_run_record = run_records[0]  # Get the most recent run record
+        yield RunRequest(run_key=latest_run_record.dagster_run.run_id)
+
+    # for run_record in run_records:
+    #     yield RunRequest(run_key=run_record.dagster_run.run_id)
+
