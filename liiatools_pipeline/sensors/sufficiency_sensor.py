32f98724596e5046a8c3a84d210e14625dd96e55
MichaelHanksSF <72440606+MichaelHanksSF@users.noreply.github.com>
2024-07-24 09:38:16 +0100

    update sufficiency_sensor to check for correct job

--- liiatools_pipeline/sensors/sufficiency_sensor.py
+++ liiatools_pipeline/sensors/sufficiency_sensor.py
@@ -1,13 +1,12 @@
 from dagster import RunRequest, RunsFilter, DagsterRunStatus, sensor
-from liiatools_pipeline.jobs.ssda903_la import ssda903_clean
-from liiatools_pipeline.jobs.ssda903_org import ssda903_sufficiency
+from liiatools_pipeline.jobs.ssda903_org import ssda903_reports, ssda903_sufficiency
 
 
 @sensor(job=ssda903_sufficiency)
 def sufficiency_sensor(context):
     run_records = context.instance.get_run_records(
         filters=RunsFilter(
-            job_name=ssda903_clean.name,
+            job_name=ssda903_reports.name,
             statuses=[DagsterRunStatus.SUCCESS],
         ),
         order_by="update_timestamp",
