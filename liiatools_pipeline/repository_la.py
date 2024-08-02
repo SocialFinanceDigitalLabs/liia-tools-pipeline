cd05b01b2a402665937865e7778e604f42454192
Matthew Pugh <pughmds@cyram.org>
2024-08-02 18:04:02 +0100

    Adjustments to enable multi-account pipelines

--- liiatools_pipeline/repository_la.py
+++ liiatools_pipeline/repository_la.py
@@ -2,12 +2,10 @@ from dagster import repository
 from liiatools.common._fs_serializer import register
 
 from liiatools_pipeline.jobs.ssda903_la import (
-    ssda903_clean,
-    ssda903_move_current,
-    ssda903_concatenate,
+    ssda903_move_current,   ## Where is this now?
     ssda903_fix_episodes,
 )
-from liiatools_pipeline.jobs.common_la import move_current, concatenate
+from liiatools_pipeline.jobs.common_la import move_current, concatenate, clean
 from liiatools_pipeline.sensors.common import concatenate_sensor, move_current_sensor
 from liiatools_pipeline.sensors.location_sensor import location_sensor
 
@@ -22,7 +20,7 @@ def sync():
     For hints on building your Dagster repository, see our documentation overview on Repositories:
     https://docs.dagster.io/overview/repositories-workspaces/repositories
     """
-    jobs = [ssda903_clean, ssda903_fix_episodes, move_current, concatenate]
+    jobs = [clean, ssda903_fix_episodes, move_current, concatenate]
     schedules = []
     sensors = [location_sensor, concatenate_sensor, move_current_sensor]
 
