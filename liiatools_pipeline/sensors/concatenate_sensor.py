de23bc04aa8052dfab1343bed6b4196d8e88c3fa
disha.javur <disha.javur@socialfinance.org.uk>
2024-08-02 10:52:10 +0100

    added default_status to running for concatenate_senor and move_current_sensor

--- liiatools_pipeline/sensors/concatenate_sensor.py
+++ liiatools_pipeline/sensors/concatenate_sensor.py
@@ -1,8 +1,12 @@
-from dagster import RunRequest, RunsFilter, DagsterRunStatus, sensor
+from dagster import RunRequest, RunsFilter, DagsterRunStatus, sensor, DefaultSensorStatus
 from liiatools_pipeline.jobs.common_la import move_current, concatenate
 
 
-@sensor(job=concatenate)
+@sensor(
+    job=concatenate,
+    description="Runs concatenate job once move_current job is complete",
+    default_status=DefaultSensorStatus.RUNNING,
+)
 def concatenate_sensor(context):
     run_records = context.instance.get_run_records(
         filters=RunsFilter(
@@ -16,7 +20,4 @@ def concatenate_sensor(context):
     if run_records:  # Ensure there is at least one run record
         latest_run_record = run_records[0]  # Get the most recent run record
         yield RunRequest(run_key=latest_run_record.dagster_run.run_id)
-
-    # for run_record in run_records:
-    #     yield RunRequest(run_key=run_record.dagster_run.run_id)
 
