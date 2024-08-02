acde8e7ece7f336cd011642a6203cf036d042250
Matthew Pugh <pughmds@cyram.org>
2024-08-02 18:10:24 +0100

    Moved some sensors to common library

--- liiatools_pipeline/sensors/move_current_sensor.py
+++
@@ -1,8 +0,0 @@
-from dagster import (
-    RunRequest,
-    RunsFilter,
-    DagsterRunStatus,
-    sensor,
-    DefaultSensorStatus,
-)
-from liiatools_pipeline.jobs.common_la import clean, move_current
