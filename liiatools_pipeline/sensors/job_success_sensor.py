188dc9376d01a235db3f6d873c838ccccc4a8d81
patrick-troy <patricktroy94@gmail.com>
2024-09-06 14:41:07 +0100

    update sensors to run through all datasets

--- liiatools_pipeline/sensors/job_success_sensor.py
+++ liiatools_pipeline/sensors/job_success_sensor.py
@@ -19,14 +19,14 @@ from liiatools_pipeline.jobs.ssda903_org import ssda903_sufficiency
 from liiatools_pipeline.ops.common_config import CleanConfig
 
 
-def find_previous_matching_ssda903_run(run_records):
+def find_previous_matching_dataset_run(run_records, dataset):
     previous_run_id = [
         run.dagster_run.run_id
         for run in run_records
-        if "ssda903" == run.dagster_run.tags["dataset"]
+        if dataset == run.dagster_run.tags["dataset"]
     ]
 
-    previous_run_id = previous_run_id[0]
+    previous_run_id = previous_run_id[0] if previous_run_id else None
     return previous_run_id
 
 
@@ -46,14 +46,16 @@ def move_current_sensor(context):
     )
 
     if run_records:  # Ensure there is at least one run record
-        latest_run_record = run_records[0]  # Get the most recent run record
-        dataset = latest_run_record.dagster_run.run_config["ops"]["process_files"][
-            "config"
-        ]["dataset"]
-        yield RunRequest(
-            run_key=latest_run_record.dagster_run.run_id,
-            tags={"dataset": dataset},
-        )
+        allowed_datasets = env_config("ALLOWED_DATASETS").split(",")
+        for dataset in allowed_datasets:
+            latest_run_id = find_previous_matching_dataset_run(
+                run_records,
+                dataset,
+            )  # Get the most recent dataset run id
+            yield RunRequest(
+                run_key=latest_run_id,
+                tags={"dataset": dataset},
+            )
 
 
 @sensor(
@@ -72,21 +74,25 @@ def concatenate_sensor(context):
     )
 
     if run_records:  # Ensure there is at least one run record
-        latest_run_record = run_records[0]  # Get the most recent run record
-        dataset = latest_run_record.dagster_run.tags["dataset"]
-        concat_config = CleanConfig(
-            dataset=dataset,
-        )
-        yield RunRequest(
-            run_key=latest_run_record.dagster_run.run_id,
-            run_config=RunConfig(
-                ops={
-                    "open_current": concat_config,
-                    "create_concatenated_view": concat_config,
-                }
-            ),
-            tags={"dataset": dataset},
-        )
+        allowed_datasets = env_config("ALLOWED_DATASETS").split(",")
+        for dataset in allowed_datasets:
+            latest_run_id = find_previous_matching_dataset_run(
+                run_records,
+                dataset,
+            )  # Get the most recent dataset run id
+            concat_config = CleanConfig(
+                dataset=dataset,
+            )
+            yield RunRequest(
+                run_key=latest_run_id,
+                run_config=RunConfig(
+                    ops={
+                        "open_current": concat_config,
+                        "create_concatenated_view": concat_config,
+                    }
+                ),
+                tags={"dataset": dataset},
+            )
 
 
 @sensor(
@@ -104,10 +110,11 @@ def ssda903_fix_episodes_sensor(context):
         ascending=False,
     )
 
-    if run_records:  # Ensure there is at least one run record
-        latest_run_id = find_previous_matching_ssda903_run(
-            run_records
-        )  # Get the most recent ssda903 run record
+    latest_run_id = find_previous_matching_dataset_run(
+        run_records,
+        "ssda903"
+    )  # Get the most recent ssda903 run id
+    if latest_run_id:  # Ensure there is at least one ssda903 run record
         yield RunRequest(run_key=latest_run_id, run_config=RunConfig())
 
 
@@ -149,11 +156,12 @@ def move_current_and_concat_sensor(context):
     )
 
     if run_records:  # Ensure there is at least one run record
-        latest_run_record = run_records[0]  # Get the most recent run record
-
         allowed_datasets = env_config("ALLOWED_DATASETS").split(",")
-        context.log.info(f"Allowed datasets: {allowed_datasets}")
         for dataset in allowed_datasets:
+            latest_run_id = find_previous_matching_dataset_run(
+                run_records,
+                dataset,
+            )  # Get the most recent dataset run id
             clean_config = CleanConfig(
                 dataset_folder=None,
                 la_folder=None,
@@ -161,7 +169,7 @@ def move_current_and_concat_sensor(context):
                 dataset=dataset,
             )
             yield RunRequest(
-                run_key=latest_run_record.dagster_run.run_id,
+                run_key=latest_run_id,
                 run_config=RunConfig(
                     ops={
                         "move_current_and_concat_view": clean_config,
@@ -185,8 +193,11 @@ def sufficiency_sensor(context):
         ascending=False,
     )
 
-    if run_records:  # Ensure there is at least one run record
-        latest_run_record = run_records[0]  # Get the most recent run record
+    latest_run_id = find_previous_matching_dataset_run(
+        run_records,
+        "ssda903"
+    )  # Get the most recent ssda903 run id
+    if latest_run_id:  # Ensure there is at least one ssda903 run record
         yield RunRequest(
-            run_key=latest_run_record.dagster_run.run_id,
+            run_key=latest_run_id,
         )
