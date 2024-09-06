1a1dfada38402a994acb2e23d649ac3571cff23b
Matthew Pugh <pughmds@cyram.org>
2024-09-06 14:43:46 +0100

    Removed unused imports on split

--- liiatools_pipeline/repository_org.py
+++ liiatools_pipeline/repository_org.py
@@ -1,27 +1,17 @@
 from dagster import repository
 from liiatools.common._fs_serializer import register
 
-from liiatools_pipeline.jobs.common_la import (
-    clean,
-    move_current,
-    concatenate,
-)
 from liiatools_pipeline.jobs.common_org import (
     move_error_reports,
     move_current_and_concat,
     reports,
 )
 from liiatools_pipeline.jobs.ssda903_org import ssda903_sufficiency
-from liiatools_pipeline.jobs.ssda903_la import ssda903_fix_episodes
 from liiatools_pipeline.jobs.external_dataset import external_incoming
 from liiatools_pipeline.sensors.location_schedule import (
-    clean_schedule,
     reports_schedule,
 )
 from liiatools_pipeline.sensors.job_success_sensor import (
-    move_current_sensor,
-    concatenate_sensor,
-    ssda903_fix_episodes_sensor,
     move_error_reports_sensor,
     move_current_and_concat_sensor,
     sufficiency_sensor,
