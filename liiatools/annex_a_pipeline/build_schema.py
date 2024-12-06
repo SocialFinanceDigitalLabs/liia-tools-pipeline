991a74b6e199a256108d1d8e697219acc6c0e81a
AmyNickollse <amy.nickolls@googlemail.com>
2024-12-06 17:57:01 +0000

    build_schema finished

--- liiatools/annex_a_pipeline/build_schema.py
+++ liiatools/annex_a_pipeline/build_schema.py
@@ -1,12 +1,15 @@
 import pandas as pd
-import yaml
+from ruamel.yaml import YAML
+
+yaml = YAML()
+yaml.preserve_quotes = True
+yaml.indent(mapping=4, sequence=6, offset=2)
 
 
-def build_schema(schema_path, excel_path, output_schema_path):
-    
+def build_schema(schema_path, excel_path, output_schema_path):   
     # Load the YAML schema
     with open(schema_path, 'r') as file:
-        schema = yaml.safe_load(file)
+        schema = yaml.load(file)
 
     # Load the Excel file with codes
     excel_data = pd.ExcelFile(excel_path)
@@ -36,13 +39,26 @@ def build_schema(schema_path, excel_path, output_schema_path):
             for _, row in grouped.iterrows()
         ]
 
+        # Start a count to know if any items in the schema have been updated
+        updated_count = 0
+
         # Check if the tab matches a column name in the schema and only update category under that column
         for annex_a_list, columns in schema.get("column_map", {}).items():
+            
             for column_name, column_coding in columns.items():
-                if isinstance(column_coding, dict) and column_coding.get("category") and tab in column_name.lower():
+                tab_name = tab.replace(' ', '-').lower()
+                anchor = column_coding.anchor.value
+                
+                if isinstance(column_coding, dict) and anchor is not None and column_coding.get("category") and tab_name == anchor:
+                    print(tab_name)
+                    print(anchor)
                     # Replace the old categories with the new ones
                     column_coding["category"] = categories
+                    updated_count += 1
+
+        if updated_count < 0:
+            print(f"Skipping tab '{tab}' as it does not have a matching item in the schema")
 
     # Save the updated schema back to a YAML file
     with open(output_schema_path, 'w') as file:
-        yaml.dump(schema, file, default_flow_style=False, allow_unicode=True, indent=4)
+        yaml.dump(schema, file)
