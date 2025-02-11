8ab5ff00839f06e09586af8c37041282a8b8d93c
AmyNickollse <amy.nickolls@googlemail.com>
2025-02-11 14:37:45 +0000

    convert_column_header_to_match moved to common

--- liiatools/annex_a_pipeline/stream_pipeline.py
+++ liiatools/annex_a_pipeline/stream_pipeline.py
@@ -2,7 +2,6 @@ import logging
 
 from sfdata_stream_parser.filters import generic
 
-from liiatools.annex_a_pipeline import stream_filters
 from liiatools.common import stream_filters as stream_functions
 from liiatools.common.data import DataContainer, FileLocator, ProcessResult
 from liiatools.common.spec.__data_schema import DataSchema
@@ -26,7 +25,7 @@ def task_cleanfile(src_file: FileLocator, schema: DataSchema) -> ProcessResult:
     # Configure stream
     stream = stream_functions.add_table_name(stream, schema=schema)
     stream = stream_functions.inherit_property(stream, ["table_name", "table_spec"])
-    stream = stream_filters.convert_column_header_to_match(stream, schema=schema)
+    stream = stream_functions.convert_column_header_to_match(stream, schema=schema)
     stream = stream_functions.match_config_to_cell(stream, schema=schema)
 
     logger.info("Stream for file %s configured", src_file.name)
