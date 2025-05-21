d83fa1e519f84a28e0f8e80b622ce3b7b4e6bb04
patrick-troy <patricktroy94@gmail.com>
2025-05-21 17:29:43 +0100

    turns all blanks pd.NA, np.nan, None, into empty string during to_integer

---
+++ liiatools/tests/common/test_transform_functions.py
@@ -0,0 +1,36 @@
+import unittest
+import pandas as pd
+import numpy as np
+
+from liiatools.common.data import ColumnConfig
+from liiatools.common._transform_functions import to_integer
+
+class TestToInteger(unittest.TestCase):
+    def setUp(self):
+        self.column_config = ColumnConfig(id="test_column", type="integer")
+        self.metadata = {"metadata": "test"}
+
+    def test_to_integer_with_valid_number(self):
+        row = pd.Series({"test_column": "123.0"})
+        result = to_integer(row, self.column_config, self.metadata)
+        self.assertEqual(result, 123)
+
+    def test_to_integer_with_invalid_number(self):
+        row = pd.Series({"test_column": "abc"})
+        result = to_integer(row, self.column_config, self.metadata)
+        self.assertEqual(result, "abc")
+
+    def test_to_integer_with_nan(self):
+        row = pd.Series({"test_column": np.nan})
+        result = to_integer(row, self.column_config, self.metadata)
+        self.assertEqual(result, "")
+
+    def test_to_integer_with_na(self):
+        row = pd.Series({"test_column": pd.NA})
+        result = to_integer(row, self.column_config, self.metadata)
+        self.assertEqual(result, "")
+
+    def test_to_integer_with_empty_value(self):
+        row = pd.Series({"test_column": None})
+        result = to_integer(row, self.column_config, self.metadata)
+        self.assertEqual(result, "")
