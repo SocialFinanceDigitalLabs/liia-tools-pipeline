9fa44a69ec2ec19fc50ecff29106a47e985fdeb7
patrick-troy <patricktroy94@gmail.com>
2025-06-12 13:04:16 +0300

    update regex

--- liiatools/common/pipeline.py
+++ liiatools/common/pipeline.py
@@ -130,8 +130,8 @@ def move_files_for_sharing(
                     dest_path = file_path.split("/")[-1]
                     copy_file(source_fs, file_path, destination_fs, dest_path)
                 else:
-                    table_id = re.search(r"\d{4}_(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)*_*([a-zA-Z0-9_]*)\.", file_path)
-                    if table_id and table_id.group(2) in required_table_id:
+                    table_id = re.search(r"(([a-zA-Z0-9]*)_([a-zA-Z0-9]*))\.", file_path)
+                    if table_id and table_id.group(3) in required_table_id or table_id and table_id.group(1) in required_table_id:
                         dest_path = file_path.split("/")[-1]
                         copy_file(source_fs, file_path, destination_fs, dest_path)
             except Exception as e:
