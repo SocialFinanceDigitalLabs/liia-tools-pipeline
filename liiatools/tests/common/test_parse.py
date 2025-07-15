cba92d6232f96180c7805c14a48c7528dd3c7221
patrick-troy <patricktroy94@gmail.com>
2025-07-15 15:56:47 +0100

    update test_parse

--- liiatools/tests/common/test_parse.py
+++ liiatools/tests/common/test_parse.py
@@ -20,22 +20,15 @@ def test_dom_parse():
     )
 
     events = [(type(event), event.as_dict()) for event in stream]
-
     assert events[0][0] == StartElement
     assert (
         events[0][1].items()
         >= {"attrib": {}, "filename": "csww.xml", "tag": "a"}.items()
     )
 
-    assert events[1][0] == TextNode
+    assert events[1][0] == ProcessingInstructionNode
     assert (
         events[1][1].items()
-        >= {"cell": None, "filename": "csww.xml", "text": None}.items()
-    )
-
-    assert events[2][0] == ProcessingInstructionNode
-    assert (
-        events[2][1].items()
         >= {
             "cell": "PIContent",
             "filename": "csww.xml",
@@ -44,68 +37,62 @@ def test_dom_parse():
         }.items()
     )
 
-    assert events[3][0] == StartElement
+    assert events[2][0] == StartElement
     assert (
-        events[3][1].items()
+        events[2][1].items()
         >= {"attrib": {}, "filename": "csww.xml", "tag": "b"}.items()
     )
 
-    assert events[4][0] == TextNode
+    assert events[3][0] == StartElement
+    assert (
+        events[3][1].items()
+        >= {"attrib": {"frog": "f"}, "filename": "csww.xml", "tag": "c"}.items()
+    )
+
+    assert events[4][0] == CommentNode
     assert (
         events[4][1].items()
-        >= {"cell": "1", "filename": "csww.xml", "text": None}.items()
+        >= {"cell": " yeah ", "filename": "csww.xml", "text": None}.items()
     )
 
     assert events[5][0] == StartElement
     assert (
         events[5][1].items()
-        >= {"attrib": {"frog": "f"}, "filename": "csww.xml", "tag": "c"}.items()
-    )
-
-    assert events[6][0] == TextNode
-    assert (
-        events[6][1].items()
-        >= {"cell": "2a", "filename": "csww.xml", "text": None}.items()
-    )
-
-    assert events[7][0] == CommentNode
-    assert (
-        events[7][1].items()
-        >= {"cell": " yeah ", "filename": "csww.xml", "text": None}.items()
-    )
-
-    assert events[8][0] == StartElement
-    assert (
-        events[8][1].items()
         >= {"attrib": {}, "filename": "csww.xml", "tag": "d"}.items()
     )
 
-    assert events[9][0] == TextNode
-    assert (
-        events[9][1].items()
-        >= {"cell": None, "filename": "csww.xml", "text": None}.items()
-    )
+    assert events[6][0] == EndElement
+    assert events[6][1].items() >= {"filename": "csww.xml", "tag": "d"}.items()
 
-    assert events[10][0] == EndElement
-    assert events[10][1].items() >= {"filename": "csww.xml", "tag": "d"}.items()
-
-    assert events[11][0] == TextNode
+    assert events[7][0] == TextNode
     assert (
-        events[11][1].items()
+        events[7][1].items()
         >= {"cell": "3", "filename": "csww.xml", "text": None}.items()
     )
 
-    assert events[12][0] == EndElement
-    assert events[12][1].items() >= {"filename": "csww.xml", "tag": "c"}.items()
-
-    assert events[13][0] == EndElement
-    assert events[13][1].items() >= {"filename": "csww.xml", "tag": "b"}.items()
-
-    assert events[14][0] == TextNode
+    assert events[8][0] == TextNode
     assert (
-        events[14][1].items()
+        events[8][1].items()
+        >= {"cell": "2a", "filename": "csww.xml", "text": None}.items()
+    )
+
+    assert events[9][0] == EndElement
+    assert events[9][1].items() >= {"filename": "csww.xml", "tag": "c"}.items()
+
+    assert events[10][0] == TextNode
+    assert (
+        events[10][1].items()
+        >= {"cell": "1", "filename": "csww.xml", "text": None}.items()
+    )
+
+    assert events[11][0] == EndElement
+    assert events[11][1].items() >= {"filename": "csww.xml", "tag": "b"}.items()
+
+    assert events[12][0] == TextNode
+    assert (
+        events[12][1].items()
         >= {"cell": "4", "filename": "csww.xml", "text": None}.items()
     )
 
-    assert events[15][0] == EndElement
-    assert events[15][1].items() >= {"filename": "csww.xml", "tag": "a"}.items()
+    assert events[13][0] == EndElement
+    assert events[13][1].items() >= {"filename": "csww.xml", "tag": "a"}.items()
