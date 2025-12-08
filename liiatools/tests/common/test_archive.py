7c1db67f30302cf20e8444926db333fbdc19b73e
MichaelHanksSF <michael.hanks@socialfinance.org.uk>
2025-12-08 17:22:42 +0000

    fix tests

--- liiatools/tests/common/test_archive.py
+++ liiatools/tests/common/test_archive.py
@@ -62,7 +62,7 @@ def test_archive(archive: DataframeArchive):
             ]
         ),
     }
-    archive.add(dataset, la_code, year, month=None, term=None, identifier=None)
+    archive.add(dataset, la_code, year, month=None, term=None, school_type=None, identifier=None)
 
     snapshots = archive.list_snapshots()
     assert snapshots == {
@@ -80,7 +80,7 @@ def test_combine(archive: DataframeArchive):
     dataset = {
         "table1": pd.DataFrame([{"id": 1, "name": "foo"}, {"id": 2, "name": "bar"}]),
     }
-    archive.add(dataset, la_code, year, month=None, term=None, identifier=None)
+    archive.add(dataset, la_code, year, month=None, term=None, school_type=None, identifier=None)
     current = archive.current(la_code)
     table_1 = current["table1"]
 
@@ -90,7 +90,7 @@ def test_combine(archive: DataframeArchive):
     dataset = {
         "table1": pd.DataFrame([{"id": 4, "name": "SNAFU"}]),
     }
-    archive.add(dataset, la_code, year, month=None, term=None, identifier=None)
+    archive.add(dataset, la_code, year, month=None, term=None, school_type=None, identifier=None)
     current = archive.current(la_code)
     table_1 = current["table1"]
 
