1a766219c776b2a937833a0f7f83d081978bcbe5
teigan-berre <teigan.berre@walthamforest.gov.uk>
2025-12-12 14:55:15 +0000

    adding init, stream filters, stream pipeline

---
+++ liiatools/sen2_pipeline/spec/__init__.py
@@ -0,0 +1,46 @@
+import importlib.resources
+import logging
+from functools import lru_cache
+from pathlib import Path
+
+import xmlschema
+import yaml
+from pydantic_yaml import parse_yaml_file_as
+
+from liiatools.common.data import PipelineConfig
+from liiatools.common.spec import load_region_env
+
+logger = logging.getLogger(__name__)
+
+SCHEMA_DIR = Path(__file__).parent
+
+region_config = load_region_env()
+
+
+@lru_cache
+def load_pipeline_config():
+    try:
+        with importlib.resources.open_text(
+            f"{region_config}_pipeline_config", "sen2_pipeline.json"
+        ) as f:
+            return parse_yaml_file_as(PipelineConfig, f)
+    except ModuleNotFoundError:
+        logger.info(f"Configuration region '{region_config}' not found.")
+    except FileNotFoundError:
+        logger.info(
+            f"Configuration file 'sen2_pipeline.json' not found in '{region_config}'."
+        )
+
+
+@lru_cache
+def load_schema(year: int) -> (xmlschema.XMLSchema, Path):
+    return (
+        xmlschema.XMLSchema(SCHEMA_DIR / f"sen2_schema_{year:04d}.xsd"),
+        Path(SCHEMA_DIR, f"sen2_schema_{year:04d}.xsd"),
+    )
+
+
+@lru_cache
+def load_reports():
+    with open(SCHEMA_DIR / "reports.yml", "rt") as FILE:
+        return yaml.load(FILE, Loader=yaml.FullLoader)
